72b4f80e22a76ae796ae8f34cd05e562f9d707e5
Clea F. Rees <ReesC21@cardiff.ac.uk>
2024-08-13 02:19:27 +0100

    add romandeadf draft with fonts, tests etc.

---
+++ arkandis/romandeadf/build.lua
@@ -0,0 +1,57 @@
+-- $Id: build.lua 10234 2024-08-13 01:16:07Z cfrees $
+-- Build configuration for romandeadf
+-- l3build.pdf listing 1 tudalen 9
+--[[
+	os.setenv requires shell-escape (which l3build always enables) but will 
+	*appear** to set the variable anyway i.e will report the value even though
+	it isn't set
+	os.execute("env") can be used to show the environment
+	os.setenv is luatex and not in the standard builtin os lua library
+	ref. https://tex.stackexchange.com/questions/720446/how-can-i-export-variables-to-the-environment-when-running-l3build?noredirect=1#comment1791863_720446
+--]]
+os.setenv ("PATH", "/usr/local/texlive/bin:/usr/bin:")
+os.setenv ("TEXMFHOME", ".")
+os.setenv ("TEXMFLOCAL", ".")
+os.setenv ("TEXMFARCH", ".")
+--
+ctanpkg = "romande"
+maindir = "../.."
+module = "romande"
+vendor = "arkandis"
+autotestfds = {  "t1yrd.fd", "t1yrda.fd", "t1yrdaw.fd", "t1yrdw.fd" }
+dofile(maindir .. "/fontinst.lua")
+local srcfiles = { "ts1-euro.etx", "ts1-euro.enc" }
+for i,j in ipairs(srcfiles) do table.insert(sourcefiles,j) end
+typesetruns = 5
+--
+uploadconfig = {
+  -- *required* --
+  -- announcement (don't include here?)
+	author     = "Hirwen Harendal; Clea F. Rees",
+  -- email (don't include here!)
+	ctanPath   = "fonts/romande",
+	license    = {"lppl1.3c","GPL 2 with font exception"},
+	pkg        = ctanpkg,
+	summary    = "Support for LibrisADF on 8-bit engines",
+  uploader   = "Clea F. Rees",
+	version    = "v2.1",
+  -- optional --
+	bugtracker = {"https://codeberg.org/cfr/nfssext/issues"},
+  -- description
+  -- development {}
+  -- home {}
+	-- repository = {"https://codeberg.org/cfr/nfssext", "https://github.com/cfr42/nfssext"},
+	note = "Repository mirrored at https://github.com/cfr42/nfssext",
+	repository = "https://codeberg.org/cfr/nfssext",
+  -- support {}
+	topic      = {"font", "font-type1"},
+	update     = true,
+  -- files --
+  -- announcement_file
+  -- note_file
+  -- curlopt_file
+}
+--
+dofile(maindir .. "/arkandis/arkandis-manifest.lua")
+-- os.execute ("printenv")
+-- vim: ts=2:sw=2:tw=80:nospell
