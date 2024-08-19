0c55410f33960445fe5b32a23c9403d2a694360e
Clea F. Rees <ReesC21@cardiff.ac.uk>
2024-08-19 06:54:42 +0100

    split implementation out altogether and try to improve indexing a little

--- arkandis/venturisadf/build.lua
+++ arkandis/venturisadf/build.lua
@@ -1,4 +1,4 @@
--- $Id: build.lua 10241 2024-08-17 03:21:52Z cfrees $
+-- $Id: build.lua 10250 2024-08-19 00:52:13Z cfrees $
 -- Build configuration for venturisadf
 -- l3build.pdf listing 1 tudalen 9
 --[[
@@ -19,6 +19,7 @@ maindir = "../.."
 module = "venturisadf"
 vendor = "arkandis"
 autotestfds = { "t1yvt.fd", "t1yvtajw.fd", "t1yvtaw.fd", "t1yvtd.fd", "t1yvtj.fd", "t1yvtjw.fd", "t1yvtw.fd", "t1yv2.fd", "t1yvo.fd", "t1yvoa.fd", "t1yvoad.fd", "t1yvod.fd", "t1yv1.fd", "t1yv1d.fd", "t1yv3.fd" }
+typesetfiles = { module .. ".dtx", "*-imp.dtx", "*-example.tex", "*-tables.tex" }
 dofile(maindir .. "/fontinst.lua")
 local srcfiles = { "lining.etx", "oldstyle.etx", "t1-dotalt-f_f.enc", "t1-dotalt-f_f.etx", "t1-f_f.enc", "t1-f_f.etx", "t1j-f_f.etx", "ts1-euro.enc", "ts1-euro.etx", "ucdotalt.etx" }
 for i,j in ipairs(srcfiles) do table.insert(sourcefiles,j) end
