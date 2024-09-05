9052a9f891daf418d0e1febab6f83595802d5680
Clea F. Rees <ReesC21@cardiff.ac.uk>
2024-09-05 05:41:20 +0100

    add ts1 for swash so tc commands find characters when using ly1 encodings (copy of non-swash fd files with string substitutions), update auto test tlg files

--- arkandis/berenisadf/build.lua
+++ arkandis/berenisadf/build.lua
@@ -1,4 +1,4 @@
--- $Id: build.lua 10317 2024-09-04 07:40:12Z cfrees $
+-- $Id: build.lua 10318 2024-09-05 03:04:12Z cfrees $
 -- Build configuration for berenisadf
 -- l3build.pdf listing 1 tudalen 9
 --[[
@@ -13,14 +13,25 @@ os.setenv ("PATH", "/usr/local/texlive/bin:/usr/bin:")
 os.setenv ("TEXMFHOME", ".")
 os.setenv ("TEXMFLOCAL", ".")
 os.setenv ("TEXMFARCH", ".")
---
+-------------------------------------------------
+-- copy non-public things from l3build
+local os_newline_cp = "\n"
+if os.type == "windows" then
+  if tonumber(status.luatex_version) < 100 or
+     (tonumber(status.luatex_version) == 100
+       and tonumber(status.luatex_revision) < 4) then
+    os_newline_cp = "\r\n"
+  end
+end
+-------------------------------------------------
 ctanpkg = "berenisadf"
 maindir = "../.."
 module = "berenis"
 vendor = "arkandis"
 autotestfds = {  "ly1ybd.fd", "ly1ybd2.fd", "ly1ybd2j.fd", "ly1ybd2jw.fd", "ly1ybd2w.fd", "ly1ybdj.fd", "ly1ybdjw.fd", "ly1ybdw.fd", "t1ybd.fd", "t1ybd2.fd", "t1ybd2j.fd", "t1ybdj.fd" }
-keepfiles = { "ybd.map", "*.afm", "*.pfb", "*.tfm" }
+keepfiles = { "ybd.map", "*.afm", "*.pfb", "*.tfm" , "ts1ybd2w.fd", "ts1ybd2jw.fd", "ts1ybdjw.fd", "ts1ybdw.fd" }
 keeptempfiles = { "*.pl" }
+autotcfds ={ "ts1ybd2j.fd", "ts1ybd2.fd", "ts1ybdj.fd", "ts1ybd.fd" }
 dofile(maindir .. "/fontinst.lua")
 function fntmake (dir,mode)
   dir = dir or unpackdir
@@ -35,6 +46,32 @@ function fntmake (dir,mode)
     errorlevel = run(dir, "make -f Makefile.make all")
     if errorlevel ~= 0 then
       gwall("make ", unpackdir, errorlevel)
+    end
+    -- make ts1 swash families so tc commands pick up the characters in ly1
+    -- we don't need t1 versions of these families as there's no room for swash
+    -- there
+    -- ideally, we could just tell latex to use the non-swash families for the
+    -- tc encoding, but that doesn't seem possible without rewriting more
+    -- internal stuff than seems altogether wise ...
+    for i, j in ipairs(autotcfds) do
+      local jfam = string.gsub(j, "^ts1(.*)%.fd$", "%1")
+      local jnewfam = jfam .. "w"
+      local jnew = string.gsub(j, "(%.fd)$", "w%1")
+      local f = assert(io.open(unpackdir .. "/" .. j,"rb"))
+      local content = f:read("*all")
+      f:close()
+      -- ought to normalise line endings here 
+      -- copied from l3build
+      -- but I don't understand why the first subs is needed
+      -- is it a problem if the file doesn't end with a newline?
+      content = string.gsub(content .. (string.match(content,"\n$") and "" or "\n"), "\r\n", "\n") 
+      local new_content = string.gsub(content, "{" .. jfam .. "}", "{" .. jnewfam .. "}")
+      new_content = string.gsub(new_content, "(ts1[^%.]*)(%.fd)", "%1w%2")
+      new_content = string.gsub(new_content, "(TS1/ybd[a-z0-9]*)", "%1w")
+      f = assert(io.open(unpackdir .. "/" .. jnew,"w"))
+      -- this somehow removes the second value returned by string.gsub??
+      f:write((string.gsub(new_content,"\n",os_newline_cp)))
+      f:close()
     end
     errorlevel = fntkeeper()
     if errorlevel ~= 0 then
