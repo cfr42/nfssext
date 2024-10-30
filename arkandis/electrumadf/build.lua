c131d07501c1b2e38abc54ffd7a9fa485768112b
Clea F. Rees <ReesC21@cardiff.ac.uk>
2024-10-30 04:53:36 +0000

    try unpacking unpackdeps after unpackdir is cleaned (brute force clobbering of l3build-unpack.lua in electrumadf build.lua (if this works, electrumadf should build or error later

--- arkandis/electrumadf/build.lua
+++ arkandis/electrumadf/build.lua
@@ -54,5 +54,86 @@ uploadconfig = {
 }
 --
 dofile(maindir .. "/arkandis/arkandis-manifest.lua")
+-- beth sy'n digwydd pe taswn i'n newid ...?
+-- l3build-unpack.lua
+---------------------------------------------------------------------
+---------------------------------------------------------------------
+-- Unpack the package files using an 'isolated' system: this requires
+-- a copy of the 'basic' DocStrip program, which is used then removed
+function unpack(sources, sourcedirs)
+  local errorlevel = dep_install(unpackdeps)
+  if errorlevel ~= 0 then
+    return errorlevel
+  end
+  errorlevel = bundleunpack(sourcedirs, sources)
+  if errorlevel ~= 0 then
+    return errorlevel
+  end
+  for _,i in ipairs(installfiles) do
+    errorlevel = cp(i, unpackdir, localdir)
+    if errorlevel ~= 0 then
+      return errorlevel
+    end
+  end
+  return 0
+end
+
+-- Split off from the main unpack so it can be used on a bundle and not
+-- leave only one modules files
+function bundleunpack(sourcedirs, sources)
+  local errorlevel = mkdir(localdir)
+  if errorlevel ~=0 then
+    return errorlevel
+  end
+  errorlevel = cleandir(unpackdir)
+  if errorlevel ~=0 then
+    return errorlevel
+  end
+  local errorlevel = dep_install(unpackdeps)
+  if errorlevel ~= 0 then
+    return errorlevel
+  end
+  for _,i in ipairs(sourcedirs or {sourcefiledir}) do
+    for _,j in ipairs(sources or {sourcefiles}) do
+      for _,k in ipairs(j) do
+        errorlevel = cp(k, i, unpackdir)
+        if errorlevel ~=0 then
+          return errorlevel
+        end
+      end
+    end
+  end
+  for _,i in ipairs(unpacksuppfiles) do
+    errorlevel = cp(i, supportdir, localdir)
+    if errorlevel ~=0 then
+      return errorlevel
+    end
+  end
+  local popen = io.popen
+  for _,i in ipairs(unpackfiles) do
+    for _,p in ipairs(tree(unpackdir, i)) do
+      local path, name = splitpath(p.src)
+      local localdir = abspath(localdir)
+      local success = assert(popen(
+        "cd " .. unpackdir .. "/" .. path .. os_concat ..
+        os_setenv .. " TEXINPUTS=." .. os_pathsep
+          .. localdir .. (unpacksearch and os_pathsep or "") ..
+        os_concat  ..
+        os_setenv .. " LUAINPUTS=." .. os_pathsep
+          .. localdir .. (unpacksearch and os_pathsep or "") ..
+        os_concat ..
+        unpackexe .. " " .. unpackopts .. " " .. name
+          .. (options["quiet"] and (" > " .. os_null) or ""),
+        "w"
+      ):write(string.rep("y\n", 300))):close()
+      if not success then
+        return 1
+      end
+    end
+  end
+  return 0
+end
+---------------------------------------------------------------------
+---------------------------------------------------------------------
 -- os.execute ("printenv")
 -- vim: ts=2:sw=2:tw=80:nospell
