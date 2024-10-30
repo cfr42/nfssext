dad8e1ed5a73ecd490cee1afb44cefbaa0eb96c7
Clea F. Rees <ReesC21@cardiff.ac.uk>
2024-10-30 02:52:53 +0000

    merge svn devel fixes to adf build scripts

--- arkandis/venturisadf/build.lua
+++ arkandis/venturisadf/build.lua
@@ -1,4 +1,4 @@
--- $Id: build.lua 10538 2024-10-28 16:07:36Z cfrees $
+-- $Id: build.lua 10545 2024-10-30 02:24:39Z cfrees $
 -- Build configuration for venturisadf
 -- l3build.pdf listing 1 tudalen 9
 --[[
