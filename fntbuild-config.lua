222650350637d352940b088f1af4451d9e4aa803
Clea F. Rees <cfr@noreply.codeberg.org>
2024-11-12 17:37:32 +0000

    mv tagging to fntbuild-config.lua

--- fntbuild-config.lua
+++ fntbuild-config.lua
@@ -1,4 +1,4 @@
--- $Id: fntbuild-config.lua 10611 2024-11-12 17:12:11Z cfrees $
+-- $Id: fntbuild-config.lua 10614 2024-11-12 17:37:07Z cfrees $
 -- configuration for nfssext
 -------------------------------------------------
 sourcedir = sourcedir or "."
