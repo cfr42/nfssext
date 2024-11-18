967a43236489d45d49584b38f779b297b30e2b16
Clea F. Rees <cfr@noreply.codeberg.org>
2024-11-18 00:32:07 +0000

    adapt test to use a gentium font just to make sure we know what encoding result should be

--- test/build.lua
+++ test/build.lua
@@ -1,4 +1,4 @@
--- $Id: build.lua 10612 2024-11-12 17:21:11Z cfrees $
+-- $Id: build.lua 10644 2024-11-18 00:31:59Z cfrees $
 -- Build configuration for testadf
 ctanpkg = "testadf"
 module = "test"
@@ -46,22 +46,26 @@ function filch()
   -- equiv to:
   -- /usr/bin/mkdir -p ${srcdir}/afm && /usr/bin/cp $(kpsewhich yesr8a.afm) ${srcdir}/afm/xxxr8a.afm
   -- /usr/bin/mkdir -p ${srcdir}/type1 && /usr/bin/cp $(kpsewhich yesr8a.pfb) ${srcdir}/type1/xxxr8a.pfb
-  local afm = kpse.find_file("yesr8a","afm")
-  local pfb = kpse.find_file("yesr8a","type1 fonts")
+  -- local afm = kpse.find_file("yesr8a","afm")
+  -- local pfb = kpse.find_file("yesr8a","type1 fonts")
+  local afm = kpse.find_file("GentiumPlus-Regular","afm")
+  local pfb = kpse.find_file("GentiumPlus-Regular","type1 fonts")
   if not afm or not pfb then return 1 end
   local srcdir = sourcefiledir or "."
   local dafm = dirname(afm)
   afm = basename(afm)
-  local xafm = string.gsub(afm,"yes","xxx")
+  local xafm = string.gsub(afm,"GentiumPlus%-Regular","xxxr8a")
   local dpfb = dirname(pfb)
   pfb = basename(pfb)
-  local xpfb = string.gsub(pfb,"yes","xxx")
+  local xpfb = string.gsub(pfb,"GentiumPlus%-Regular","xxxr8a")
   local afmdir = srcdir .. "/afm"
   local pfbdir = srcdir .. "/type1"
-  local errorlevel = direxists(afmdir) 
-  if errorlevel ~=0 then mkdir(afmdir) end
-  errorlevel = direxists(pfbdir)
-  if errorlevel ~=0 then mkdir(pfbdir) end
+  cleandir(afmdir)
+  cleandir(pfbdir)
+  -- local errorlevel = direxists(afmdir) 
+  -- if errorlevel ~=0 then mkdir(afmdir) end
+  -- errorlevel = direxists(pfbdir)
+  -- if errorlevel ~=0 then mkdir(pfbdir) end
   xafm = afmdir .. "/" .. xafm
   xpfb = pfbdir .. "/" .. xpfb
   cp(afm,dafm,xafm)     
