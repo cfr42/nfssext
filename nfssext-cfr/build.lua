cbc2660054680ec83cec0925b1612ec865e7be86
Clea F. Rees <cfr@noreply.codeberg.org>
2025-01-03 18:10:07 +0000

    TeX : nfssext : nfssext-cfr : update build.lua

--- nfssext-cfr/build.lua
+++ nfssext-cfr/build.lua
@@ -1,42 +1,32 @@
--- $Id: build.lua 10705 2025-01-02 07:47:44Z cfrees $
+-- $Id: build.lua 10712 2025-01-03 18:09:59Z cfrees $
 -- Build configuration for nfssext-cfr
+-------------------------------------------------------------------------------
 -- l3build.pdf listing 1 tudalen 9
---[[
-	os.setenv requires shell-escape (which l3build always enables) but will 
-	*appear** to set the variable anyway i.e will report the value even though
-	it isn't set
-	os.execute("env") can be used to show the environment
-	os.setenv is luatex and not in the standard builtin os lua library
-	ref. https://tex.stackexchange.com/questions/720446/how-can-i-export-variables-to-the-environment-when-running-l3build?noredirect=1#comment1791863_720446
---]]
---
+-------------------------------------------------------------------------------
 module = "nfssext-cfr"
 ctanpkg = module
--- unpackfiles = {"*.ins"}
 -- maindir **must** be shared with dependencies
 maindir = ".."
 sourcefiledir = "."
--- auxfiles = {"*.aux"}
 sourcefiles = {"*.dtx","*.ins"}
 checkengines = {"pdftex"}
 checkformat = "latex"
 checksuppfiles = {"*.fd"}
 manifestfile = "manifest.txt"
--- checkdeps = {maindir .. "/cfr-lm"}
 typesetdeps = {maindir .. "/cfr-lm"}
 typesetsourcefiles = {"cfr-lm.sty", maindir .. "/cfr-lm/keep/*"}
 typesetruns = 5
---
+-------------------------------------------------------------------------------
 dofile(maindir .. "/tag.lua")
 date = "2008-2025"
 if direxists(sourcefiledir .. "/../../adnoddau/l3build") then
   dofile(sourcefiledir .. "/../../adnoddau/l3build/manifest.lua")
 end
---
+-------------------------------------------------------------------------------
 uploadconfig = {
   -- *required* --
   -- announcement (don't include here?)
-  announcement  = "",--Belated update for (N)NFSS. Implement new package options. Switch to dtx/ins. Use l3build.",
+  announcement  = "Support TS1 encoding subset declarations in font definition files for LaTeX format versions prior to the 2024-11-19 hot fix. For pre-2020 versions, the code merely avoids errors; for post-2020 versions, the preamble definition is saved for reuse.",
 	author     = "Clea F. Rees",
   -- email (don't include here!)
 	ctanPath   = "/macros/contrib/latex/nfssext-cfr",
@@ -44,13 +34,13 @@ uploadconfig = {
 	pkg        = ctanpkg,
 	summary    = "Extended font selection commands for LaTeX's (New) New Font Selection Scheme",
   uploader   = "Clea F. Rees",
-	version    = "v1.1",
+	version    = "v1.2",
   -- optional --
 	bugtracker = {"https://codeberg.org/cfr/nfssext/issues"},
   description= "An extension and modification of Philipp Lehman's nfssext which provides extended font selection commands modelled on those provided by LaTeX 2e.",
   -- development {}
   -- home {}
-	note       = "My apologies. I ought to have caught this.",
+	-- note       = "My apologies. I ought to have caught this.",
 	repository = {"https://codeberg.org/cfr/nfssext", "https://github.com/cfr42/nfssext"},
   -- support {}
 	topic      = {"font-sel", "font-use", "font-supp"},
@@ -60,5 +50,5 @@ uploadconfig = {
   -- note_file
   -- curlopt_file
 }
--- os.execute ("printenv")
+-------------------------------------------------------------------------------
 -- vim: ts=2:sw=2:tw=80:nospell
