4eb716b11bff35f2e6afd81baa3772d9034c71f3
Clea F. Rees <cfr@noreply.codeberg.org>
2025-02-06 00:20:40 +0000

    TeX : nfssext : arkandis : berenisadf : update build.lua

--- arkandis/berenisadf/build.lua
+++ arkandis/berenisadf/build.lua
@@ -1,24 +1,9 @@
--- $Id: build.lua 10743 2025-01-29 02:29:53Z cfrees $
--- Build configuration for berenisadf
--- l3build.pdf listing 1 tudalen 9
---[[
-	os.setenv requires shell-escape (which l3build always enables) but will 
-	*appear** to set the variable anyway i.e will report the value even though
-	it isn't set
-	os.execute("env") can be used to show the environment
-	os.setenv is luatex and not in the standard builtin os lua library
-	ref. https://tex.stackexchange.com/questions/720446/how-can-i-export-variables-to-the-environment-when-running-l3build?noredirect=1#comment1791863_720446
---]]
+-- $Id: build.lua 10773 2025-02-06 00:20:26Z cfrees $
 -------------------------------------------------
--- copy non-public things from l3build
--- local os_newline_cp = "\n"
--- if os.type == "windows" then
---   if tonumber(status.luatex_version) < 100 or
---      (tonumber(status.luatex_version) == 100
---        and tonumber(status.luatex_revision) < 4) then
---     os_newline_cp = "\r\n"
---   end
--- end
+-- Build configuration for berenisadf
+-------------------------------------------------
+-- l3build.pdf listing 1 tudalen 9
+-- ref. https://tex.stackexchange.com/questions/720446/how-can-i-export-variables-to-the-environment-when-running-l3build?noredirect=1#comment1791863_720446
 -------------------------------------------------
 ctanpkg = "berenisadf"
 maindir = "../.."
@@ -29,36 +14,40 @@ fnt.autotestfds = {  "ly1ybd.fd", "ly1ybd2.fd", "ly1ybd2j.fd", "ly1ybd2jw.fd", "
 fnt.keepfiles = { "ybd.map", "*.afm", "*.pfb", "*.tfm" , "ts1ybd2w.fd", "ts1ybd2jw.fd", "ts1ybdjw.fd", "ts1ybdw.fd" }
 fnt.keeptempfiles = { "*.pl" }
 dofile(maindir .. "/fontscripts/fntbuild.lua")
+-------------------------------------------------
 -- START doc eg
 -- require(kpse.lookup("fntbuild.lua"))
-fnt.autotcfds ={ "ts1ybd2j.fd", "ts1ybd2.fd", "ts1ybdj.fd", "ts1ybd.fd" }
 local function fntmake (dir,mode)
   dir = dir or fnt.fntdir
   mode = mode or "errorstopmode --halt-on-error"
-  fnt.buildinit()
+  local autotcfds ={ "ts1ybd2j.fd", "ts1ybd2.fd", "ts1ybdj.fd", "ts1ybd.fd" }
+  -- set up the build environment
+  assert(fnt.buildinit(), "Setting up build environment failed!")
   print("Running make. Please be patient ...\n")
-  local errorlevel = run(dir, "chmod +x ff-ybd.pe")
-  if errorlevel ~=0 then
-    fnt.gwall("Attempt to make fontforge script executable ", dir, errorlevel)
-  else
-    errorlevel = run(dir, "make -f Makefile.make all")
-    if errorlevel ~= 0 then
-      fnt.gwall("make ", dir, errorlevel)
-    end
+  -- local errorlevel = run(dir, "chmod +x ff-ybd.pe")
+  assert(run(dir, "chmod +x ff-ybd.pe"),"Attempt to make fontforge script executable in " .. dir .. " failed.")
+  -- if errorlevel ~=0 then
+    -- fnt.gwall("Attempt to make fontforge script executable ", dir, errorlevel)
+  -- else
+  assert(run(dir, "make -f Makefile.make all"), '"make -f Makefile.make all" failed in ' .. dir .. '.')
+    -- errorlevel = run(dir, "make -f Makefile.make all")
+    -- if errorlevel ~= 0 then
+      -- fnt.gwall("make ", dir, errorlevel)
+    -- end
     -- make ts1 swash families so tc commands pick up the characters in ly1
     -- we don't need t1 versions of these families as there's no room for swash
     -- there
     -- ideally, we could just tell latex to use the non-swash families for the
     -- tc encoding, but that doesn't seem possible without rewriting more
     -- internal stuff than seems altogether wise ...
-    for i, j in ipairs(fnt.autotcfds) do
+    for i, j in ipairs(autotcfds) do
       local jfam = string.gsub(j, "^ts1(.*)%.fd$", "%1")
       local jnewfam = jfam .. "w"
       local jnew = string.gsub(j, "(%.fd)$", "w%1")
       local f = assert(io.open(dir .. "/" .. j,"rb"))
       local content = f:read("*all")
       f:close()
-      -- ought to normalise line endings here 
+      -- normalise line endings to be platform-agnostic
       -- copied from l3build
       content = string.gsub(content .. (string.match(content,"\n$") and "" or "\n"),
         "\r\n", "\n"
@@ -69,24 +58,30 @@ local function fntmake (dir,mode)
       new_content = string.gsub(new_content, "(ts1[^%.]*)(%.fd)", "%1w%2")
       new_content = string.gsub(new_content, "(TS1/ybd[a-z0-9]*)", "%1w")
       f = assert(io.open(dir .. "/" .. jnew,"w"))
-      -- this somehow removes the second value returned by string.gsub??
       f:write((string.gsub(new_content,"\n",fnt.os_newline_cp)))
       f:close()
     end
-    errorlevel = fnt.fntkeeper()
-    if errorlevel ~= 0 then
-      fnt.gwall("FONT KEEPER FAILED! DO NOT MAKE STANDARD TARGETS WITHOUT RESOLVING!! ",
-        dir, errorlevel
-      )
-    end
-  end
-  return fnt.nifergwall
+    -- call fnt.fntkeeper() to save the build results into fnt.keepdir else
+    -- l3build deletes them before testing or compilation!
+    assert(fnt.fntkeeper(),"FONT KEEPER FAILED IN " .. dir .. "! DO NOT MAKE STANDARD TARGETS WITHOUT RESOLVING!! ")
+    -- errorlevel = fnt.fntkeeper()
+    -- if errorlevel ~= 0 then
+      -- fnt.gwall("FONT KEEPER FAILED! DO NOT MAKE STANDARD TARGETS WITHOUT RESOLVING!! ",
+        -- dir, errorlevel
+      -- )
+    -- end
+  -- end
+  return 0
+  -- return fnt.nifergwall
 end
-fnt.fntmake = fntmake
+-- make local function available in table bt
+bt = {}
+bt.fntmake = fntmake
+-- redefine fnt.ntarg so that fnttarg calls bt.fntmake rather than fontinst
 -- fntmake must be specified first
 -- it just ain't TeX
 target_list[fnt.ntarg] = {
-  func = fnt.fntmake,
+  func = bt.fntmake,
   desc = "Creates TeX font files",
   pre = function(names)
     if names then
@@ -98,14 +93,12 @@ target_list[fnt.ntarg] = {
   end
 }
 -- STOP doc eg
--- docfiles = { "dotoldstyle.etx", "dottaboldstyle.etx", "t1-cfr.etx", "t1-dotinferior.etx", "t1-dotsuperior.etx", "ybd-encs.tex" }
--- local srcfiles = { "dotoldstyle.etx", "dottaboldstyle.etx", "t1-cfr.etx", "t1-dotinferior.etx", "t1-dotsuperior.etx" }
--- for i,j in ipairs(srcfiles) do table.insert(sourcefiles,j) end
+-------------------------------------------------
 unpackdeps = {maindir .. "/fontscripts"}
 textfiles = {"*.md", "*.txt", "COPYING"}
 typesetdeps = {maindir .. "/nfssext-cfr"}
 typesetruns = 5
---
+-------------------------------------------------
 uploadconfig = {
   -- *required* --
   -- announcement (don't include here?)
@@ -133,10 +126,10 @@ uploadconfig = {
   -- note_file
   -- curlopt_file
 }
---
+-------------------------------------------------
 arkandisfiles = {"*.otf","NOTICE*","COPYING"}
 arkandisders = {"*.afm","*.pfb","*.pfm"}
 date = "2010-2025"
 dofile(maindir .. "/arkandis/arkandis-manifest.lua")
--- os.execute ("printenv")
+-------------------------------------------------
 -- vim: ts=2:sw=2:tw=80:nospell
