7b9af2abc4b0c66b2f743ed38b08eaa6bdaa6731
Clea F. Rees <cfr@noreply.codeberg.org>
2025-02-14 08:50:48 +0000

    TeX : nfssext : dileu fnt-test + cynnwys

--- fnt-tests/build.lua
+++
@@ -1,8 +0,0 @@
--- $Id: build.lua 10142 2024-07-08 05:33:30Z cfrees $
--- Build configuration for testadf
-module = "fnt-tests"
-maindir = ".."
-sourcefiles = {"*.tex"}
-installfiles = {"*.tex"}
-
--- vim: ts=2:sw=2:tw=80:nospell:et
