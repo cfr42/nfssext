da3635d25014f64a63233e32e279d889f053610d
Clea F. Rees <cfr@noreply.codeberg.org>
2025-06-22 23:34:53 +0100

    TeX : nfssext : nfssext-cfr : v1.3 - allow \textin for fixed hyperref

--- nfssext-cfr/build.lua
+++ nfssext-cfr/build.lua
@@ -1,4 +1,4 @@
--- $Id: build.lua 10962 2025-03-25 02:15:04Z cfrees $
+-- $Id: build.lua 11042 2025-06-22 22:34:34Z cfrees $
 -- Build configuration for nfssext-cfr
 -------------------------------------------------------------------------------
 -- l3build.pdf listing 1 tudalen 9
@@ -27,7 +27,7 @@ end
 uploadconfig = {
   -- *required* --
   -- announcement (don't include here?)
-  announcement  = "Support TS1 encoding subset declarations in font definition files for LaTeX format versions prior to the 2024-11-19 hot fix. For pre-2020 versions, the code merely avoids errors; for post-2020 versions, the preamble definition is saved for reuse.",
+  announcement  = "Allow \textin{} again for hyperref 2025-05-20 or later.",
 	author     = "Clea F. Rees",
   -- email (don't include here!)
 	ctanPath   = "/macros/contrib/latex/nfssext-cfr",
@@ -35,7 +35,7 @@ uploadconfig = {
 	pkg        = ctanpkg,
 	summary    = "Extended font selection commands for LaTeX's (New) New Font Selection Scheme",
   uploader   = "Clea F. Rees",
-	version    = "v1.2",
+	version    = "v1.3",
   -- optional --
 	bugtracker = {"https://codeberg.org/cfr/nfssext/issues"},
   description= "An extension and modification of Philipp Lehman's nfssext which provides extended font selection commands modelled on those provided by LaTeX 2e.",
